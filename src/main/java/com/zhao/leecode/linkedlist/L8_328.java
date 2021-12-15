54ab91ad2e5cbbe251ae8741e5f1dbf007a16cdf
ZhaoLeBoy <zhaolestyle@icloud.com>
2021-12-15 21:09:33 +0800

    .

--- src/main/java/com/zhao/leecode/linkedlist/L8_328.java
+++ src/main/java/com/zhao/leecode/linkedlist/L8_328.java
@@ -1,10 +1,59 @@
 package com.zhao.leecode.linkedlist;
 
+
 /**
- *328. 奇偶链表
+ * 328. 奇偶链表
  * https://leetcode-cn.com/problems/odd-even-linked-list/
+ *
  * @Author Le Zhao
  * @Date 2021/12/10 2:17 下午
  */
+
+/**
+ * 解题思路：新建两个链表以及引用， 用来存放原本链表中的奇偶数据，循环完毕后将偶数链表挂在奇数链表的最后。
+ */
 public class L8_328 {
+    public static ListNode oddEvenList(ListNode head) {
+        int step = 1;
+        ListNode nodePoint = head;
+        ListNode odd = new ListNode(1);
+        ListNode even = new ListNode(0);
+
+        ListNode oddPoint = odd;
+        ListNode evenPoint = even;
+        while (nodePoint != null) {
+            if (step % 2 == 0) {
+                //偶数
+                evenPoint.next = nodePoint;
+                evenPoint = evenPoint.next;
+            } else {
+                //奇数
+                oddPoint.next = nodePoint;
+                oddPoint = oddPoint.next;
+            }
+
+            step++;
+            nodePoint = nodePoint.next;
+        }
+        //(step - 1 )说明是偶数长度，否则是奇数长度。。偶数长度奇数链表需要-1  奇数长度偶数链表-1
+        if ((step - 1) % 2 == 0) {
+            //偶数
+            oddPoint.next = null;
+        } else {
+            //奇数
+            evenPoint.next = null;
+        }
+        oddPoint.next = even.next;
+
+        return odd.next;
+    }
+
+    public static void main(String[] args) {
+//        ListNode l5 = new ListNode(5, null);
+//        ListNode l4 = new ListNode(4, l5);
+//        ListNode l1 = new ListNode(3, l4);
+//        ListNode l2 = new ListNode(2, l1);
+        ListNode l3 = new ListNode(1, null);
+        oddEvenList(null);
+    }
 }
--- src/main/java/com/zhao/leecode/linkedlist/L9_25.java
+++ src/main/java/com/zhao/leecode/linkedlist/L9_25.java
@@ -1,5 +1,7 @@
 package com.zhao.leecode.linkedlist;
 
+
+
 /**
  * 25. K 个一组翻转链表
  * https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
@@ -7,5 +9,84 @@ package com.zhao.leecode.linkedlist;
  * @Author Le Zhao
  * @Date 2021/12/10 2:17 下午
  */
+
+/**
+ * 解题思路： 建立一个下标和一个用来存储反转的链表sub 临时链表 ，k自增 用来判断是否到K，
+ * 反转sub 挂在结果链表res中....//TODO:.只能说解决了 性能待优化
+ */
 public class L9_25 {
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        //用于接受新结果的数组和索引
+        ListNode res = new ListNode(0);
+        ListNode resPoint = res;
+
+        //用于获取需要反转的链表段
+        ListNode sub = new ListNode(0);
+        ListNode subPoint = sub;
+
+        ListNode listPoint = head;
+        int i = 0;
+        while (listPoint != null) {
+            //获取小段落链表
+            subPoint.next = new ListNode(listPoint.val);
+            subPoint = subPoint.next;
+            i++;
+
+            //获取需要反转的链表
+            if (i == k) {
+                //反转
+               ListNode reversal =  reversal(sub);
+
+                //将反转列表赋值给结果链表
+                resPoint.next = reversal;
+
+                //移动结果的指针用于
+                while (resPoint != null) {
+                    if (resPoint.next != null) {
+                        resPoint = resPoint.next;
+                    } else {
+                        break;
+                    }
+                }
+                //子链表置空，重新统计
+                sub = new ListNode(0);
+                subPoint = sub;
+                i = 0;
+            }
+
+            listPoint = listPoint.next;
+
+            //对尾巴进行收尾
+            if (listPoint == null && i < k) {
+                //
+                resPoint.next = sub.next;
+            }
+
+        }
+        return res.next;
+    }
+
+    /**
+     * 反转
+     */
+    private static ListNode reversal(ListNode node) {
+        ListNode prev = null;
+        ListNode head = node.next;
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = prev;
+            prev = head;
+            head = tmp;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode l9 = new ListNode(9, null);
+//        ListNode l10 = new ListNode(10, l9);
+//        ListNode l11 = new ListNode(11, l10);
+//        ListNode l12 = new ListNode(12, l11);
+//        ListNode l13 = new ListNode(13, l12);
+        reverseKGroup(l9, 9);
+    }
 }
